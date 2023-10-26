917c18a228782ad50a948d40084527d8a5b9c0bc
DaniAlosPrats <146813929+DaniAlosPrats@users.noreply.github.com>
2023-10-26 09:19:03 +0200

    ejer1

---
+++ ejer3/refu1.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Suma de 4 números</title>
+</head>
+<body>
+    <h1>Suma de 4 números</h1>
+    
+    <?php
+   $a = ["q", "w", "e", "r", "t", "y", "u"];
+  
+   $tam = count($a)-1;
+   $max =$tam -1;
+   while($max>= 0){
+    "echo$a[$max]" ;
+    $max--;
+   }
+   
+   
+   
+   
+   
+    
+    
+    
+
+
+    
+    
+    
+    ?>
+    
+  
+   
+</body>
+</html>

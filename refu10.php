bbe7796c5070817cda11e4bddb76a9fccbbd07a4
DaniAlosPrats <146813929+DaniAlosPrats@users.noreply.github.com>
2023-10-28 09:17:27 +0200

    ejer7 y 10

---
+++ refu10.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>lista</title>
+</head>
+<body>
+    <h1>lista</h1>
+
+    <?php
+  $array=["1", "3", "3", "8","4", "3", "2", "3", "3"];
+  
+  $tam= count($array);
+  $mitad= $tam/2;
+print_r($mitad);
+    
+    
+   
+
+
+      
+    ?>
+    
+</body>
+</html>
